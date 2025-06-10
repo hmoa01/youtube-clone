d894127809483df7e73bab0ce14fd7aa78cfdecc
hmoa01 <hmoa01@gmail.com>
2025-06-10 15:53:29 +0200

    add banner upload modal

---
+++ src/modules/users/ui/components/banner-upload-modal.tsx
@@ -0,0 +1,36 @@
+import { ResponsiveModal } from "@/components/responsive-dialog";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { trpc } from "@/trpc/client";
+
+interface BannerUploadModalProps {
+  userId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const BannerUploadModal = ({
+  userId,
+  open,
+  onOpenChange,
+}: BannerUploadModalProps) => {
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    onOpenChange(false);
+    utils.studio.getMany.invalidate();
+    utils.users.getOne.invalidate({ id: userId });
+  };
+
+  return (
+    <ResponsiveModal
+      title="Upload a banner"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <UploadDropzone
+        endpoint="bannerUploader"
+        onClientUploadComplete={onUploadComplete}
+      />
+    </ResponsiveModal>
+  );
+};
